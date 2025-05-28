2ef0af0b2712d9cccd773b73bc4b9a411b59358f
Lucian Cooper <cooperlucian@gmail.com>
2025-05-28 05:28:23 -0400

    build: add final build step to prepare package.json

---
+++ apps/server/scripts/prepare.js
@@ -0,0 +1,27 @@
+import { writeFileSync } from 'node:fs';
+import { join } from 'node:path';
+import { createRequire } from 'node:module';
+
+const require = createRequire(import.meta.url);
+
+const pkgFields = [
+    'name',
+    'version',
+    'private',
+    'type',
+    'dependencies',
+];
+
+// eslint-disable-next-line import/no-dynamic-require
+const pkg = require(join(import.meta.dirname, '../package.json'));
+
+const newPkg = Object.entries(pkg).filter(([key]) => pkgFields.includes(key)).reduce((acc, [key, value]) => {
+    acc[key] = value;
+    return acc;
+}, {
+    main: 'index.js',
+});
+
+const targetPath = join(import.meta.dirname, '../dist/package.json');
+
+writeFileSync(targetPath, JSON.stringify(newPkg, null, 2));
